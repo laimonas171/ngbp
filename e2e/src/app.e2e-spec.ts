66ff16ac5dec8713e3eba00e91f613edab99b77c
Laimonas Lizdenis <laimonas171@gmail.com>
2021-03-03 01:09:43 +0200

    updated naming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ngbp2 app is running!');
+    expect(await page.getTitleText()).toEqual('ngbp app is running!');
   });
 
   afterEach(async () => {
