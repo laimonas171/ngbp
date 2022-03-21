816fc4d8a1a42762b7e803eccbcbc4e1143b17bd
Haroldas Valciukas <haroldas.valciukas@devbridge.com>
2022-03-21 09:47:16 +0200

    editor config and e2e removal

--- apps/tumbleweed/jest.config.js
+++ apps/tumbleweed/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'tubleweed',
+  displayName: 'tumbleweed',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/tubleweed',
+  coverageDirectory: '../../coverage/apps/tumbleweed',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
