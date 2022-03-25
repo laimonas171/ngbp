a6442915e154bbf8196a0c12c4041ed6525758ae
Haroldas Valčiukas <69384036+CherryBear-hub@users.noreply.github.com>
2022-03-25 09:02:30 +0200

    Monorepo setup (#5)
    * initial commit
    
    * package versioning
    
    * update tsconfig and eslint
    
    * prettier config update
    
    * nx package scripts
    
    * README update
    
    Co-authored-by: Haroldas Valciukas <haroldas.valciukas@devbridge.com>

---
+++ apps/blank/src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode, ViewEncapsulation } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.Emulated,
+  })
+  // eslint-disable-next-line no-console
+  .catch((err) => console.error(err));
